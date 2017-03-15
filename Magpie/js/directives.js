72bb8200c608bbc03a0fcb3341b6ef616539b253
chithralekha\anupachitraprashanth <chithra@altiussystems.com>
2017-03-15 14:17:00 -0400

    no message

--- Magpie/js/directives.js
+++ Magpie/js/directives.js
@@ -224,28 +224,7 @@ function vectorMap() {
             myMapData: '=',
         },
         link: function (scope, element, attrs) {
-            var map = element.vectorMap({
-                map: 'world_mill_en',
-                backgroundColor: "transparent",
-                regionStyle: {
-                    initial: {
-                        fill: '#e4e4e4',
-                        "fill-opacity": 0.9,
-                        stroke: 'none',
-                        "stroke-width": 0,
-                        "stroke-opacity": 0
-                    }
-                },
-                series: {
-                    regions: [
-                        {
-                            values: scope.myMapData,
-                            scale: ["#1ab394", "#22d6b1"],
-                            normalizeFunction: 'polynomial'
-                        }
-                    ]
-                },
-            });
+           
             var destroyMap = function(){
                 element.remove();
             };
